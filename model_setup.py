865140582d930707bd40356993d0a23e894f25a8
Sergio Calvo <sergiocalvo@pop-os.localdomain>
2022-06-10 16:55:05 +0100

    add dataframe writing functionality

--- model_setup.py
+++ model_setup.py
@@ -6,20 +6,27 @@ from tensorflow.keras.losses import SparseCategoricalCrossentropy
 import scipy
 from scipy import optimize
 import pandas as pd
+from datetime import datetime
+
 
 from helpers import normalize_img, monoExp, powerlaw, plot_fits
 from data_loading import Data
 
 
-def build_and_compile(input_shape=(28, 28, 1), lr=0.001):
+def build_and_compile(input_shape=(28, 28, 1), optimizer=Adam, lr=0.001,
+                      loss=SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]):
     """
     Function that builds the model instance and compiles it.
-    In future work we will implement further customisation for the model architecture and compiling parameters.
+    In future work we will implement further customisation for the model architecture.
 
     :param input_shape: Input shape as a tuple for the first convolutional layer of the model. Default: (28, 28, 1).
     :type input_shape: tuple
-    :param lr: Adam() learning rate.
+    :param optimizer: Optimizer to compile the model.
+    :param lr: Learning rate.
     :type lr: float
+    :param loss: Loss used to compile the model.
+    :param metrics: Metrics to compute the performance of the model throughout training.
 
     :return: Compiled model for further use in training.
     """
@@ -42,9 +49,9 @@ def build_and_compile(input_shape=(28, 28, 1), lr=0.001):
     ])
 
     # Compile the model
-    model.compile(optimizer=Adam(lr),
-                  loss=SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+    model.compile(optimizer=optimizer(lr),
+                  loss=loss,
+                  metrics=metrics)
 
     return model
 
@@ -79,7 +86,7 @@ def step_train(model, train_data, test_data, validation_data, dts):
     return lossval
 
 
-def training_fit_loop(train_data_name: str, data_step: int, n: int, N=1, start_data=500, plot=True):
+def training_fit_loop(train_data_name: str, data_step: int, n: int, N=1, start_data=500, plot=True, save_df=False):
     """
     Function that will put together the previously defined functions with the aim of generating a training loops with n
     iterations. It will also use the results of the training experiments and fit it to both an exponential and a power
@@ -99,6 +106,8 @@ def training_fit_loop(train_data_name: str, data_step: int, n: int, N=1, start_d
     :param plot: Boolean parameter. If true then the function will plot fitting results. If false, it will just return
     the results.
     :type plot: bool
+    :param save_df: If not False, path to folder where the dataframe will be saved as a pickle.
+    :type save_df: False or str
 
     :return: R² of the fit, list of the fitting parameters, dataframe with useful information from the fitting process.
     """
@@ -172,32 +181,39 @@ def training_fit_loop(train_data_name: str, data_step: int, n: int, N=1, start_d
         decay1_array = np.array(decay1)
         r2_1_array = np.array(r2_1)
 
-        df.loc[f"Experiment {j}:"] = [r_sq_exp, (m_exp, t_exp, b_exp),
+        df.loc[f"Experiment {j+1}:"] = [r_sq_exp, (m_exp, t_exp, b_exp),
                                       np.mean(decay0_array), np.sqrt(np.var(decay0_array)),
                                       np.mean(r2_0_array),
                                       r_sq_power, (m_power, t_power, b_power),
                                       np.mean(decay1_array), np.sqrt(np.var(decay1_array)),
                                       np.mean(r2_1_array)]
-
+        
         if N > 1 and plot:
 
             if r_sq_exp < r_sq_power:
-                plot_fits(data_index_array, loss_array, [m_exp, t_exp, b_exp], "exp")
+                plot_fits(data_index_array, loss_array, [m_exp, t_exp, b_exp], "exp", experiment_number=j+1)
                 glob_params.append([m_exp, t_exp, b_exp])
 
             else:
-                plot_fits(data_index_array, loss_array, [m_power, t_power, b_power], "power")
+                plot_fits(data_index_array, loss_array, [m_power, t_power, b_power], "power", experiment_number=j+1)
                 glob_params.append([m_power, t_power, b_power])
-
+    
+    if save_df:         
+        now = datetime.now()
+        dt_string = now.strftime("%d_%m_%Y_-%H_%M_%S")
+        
+        # Save the final dataframe as a pickle
+        df.to_pickle(save_df + dt_string + f"_experiment_{j+1}")
+    
     if N > 1:
         plot = False
 
     if r_sq_exp < r_sq_power and plot:
-        plot_fits(data_index_array, loss_array, [m_exp, t_exp, b_exp], "exp")
+        plot_fits(data_index_array, loss_array, [m_exp, t_exp, b_exp], "exp", experiment_number=j+1)
         return r2_0, [m_exp, t_exp, b_exp], df
 
     elif r_sq_exp > r_sq_power and plot:
-        plot_fits(data_index_array, loss_array, [m_power, t_power, b_power], "power")
+        plot_fits(data_index_array, loss_array, [m_power, t_power, b_power], "power", experiment_number=j+1)
         return r2_1, [m_power, t_power, b_power], df
 
     elif r_sq_exp < r_sq_power:
